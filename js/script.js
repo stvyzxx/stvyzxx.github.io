d7b71b7e415ecf0acc629ca0daeddc385d390ff4
stvyzxx <yurasobechko@gmail.com>
2015-08-01 23:44:19 +0300

    e

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@
             var iOS = /iPad|iPhone|iPod/.test(navigator.platform);
             if(iOS){
                 $(".nav-list a").css("line-height", "56px");
-                $(".contacts input[type='submit']").css("line-height", "56px");
+                $(".contacts input[type='submit']").css("line-height", "50px");
             }
         }
         iosFix();
